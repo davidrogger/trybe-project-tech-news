5a2bea36ff54aed0c4c6dae975bffd3a78eeb6b1
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-08 19:58:50 -0300

    feat: scrape noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,7 +32,36 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = selector.css("head link[rel='canonical']::attr(href)").get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").re_first(r"\d{2}/\d{2}/\d{4}")
+    writer = selector.css(".author a::text").get()
+    comments_count = int(
+        selector.css(".post-comments .title-block::text").re_first(r"\d")
+        or "0"
+    )
+
+    raw_summary = selector.css(
+        ".entry-content > p:first-of-type *::text"
+    ).getall()
+
+    summary = "".join(raw_summary).strip()
+
+    tags = selector.css(".post-tags a::text").getall()
+    category = selector.css(".category-style .label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5
