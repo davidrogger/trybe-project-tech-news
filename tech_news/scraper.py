028cd472a64d557f6577e03c7b32b76182165b84
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-08 14:44:47 -0300

    feat: method fetch
    responsable to get the html from a request

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Initial commit"""
+    headers = {"user-agent": "Fake user-agent"}
+    try:
+        response = requests.get(url, headers=headers, timeout=3)
+        response.raise_for_status()
+        time.sleep(1)
+    except (requests.HTTPError, requests.ReadTimeout):
+        return None
+    else:
+        return response.text
 
 
 # Requisito 2
