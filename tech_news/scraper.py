282f8379199ffafe5a66cbb75d33646043a66ae0
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-08 15:01:05 -0300

    feat scrape next page
    responsable to get the url from the next button

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,7 +25,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page_link = selector.css(".next::attr(href)").get()
+    return next_page_link
 
 
 # Requisito 4
