8c5e26aec05f7b58bfec32c0b944902ccdb0a460
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-09 15:55:40 -0300

    feat: menu options
    add all methods need it in the menu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,14 @@
+import sys
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_tag,
+    search_by_category,
+)
+
+
 def show_options():
     print(
         "Selecione uma das opções a seguir:"
@@ -13,6 +24,69 @@ def show_options():
     )
 
 
+def end_menu():
+    print("Encerrando script\n")
+
+
+def search_titles():
+    inputed = input("Digite o título:")
+    titles_found = search_by_title(inputed)
+    print(titles_found)
+
+
+def search_dates():
+    inputed = input("Digite a data no formato aaaa-mm-dd:")
+    dates_found = search_by_date(inputed)
+    print(dates_found)
+
+
+def search_tags():
+    inputed = input("Digite a tag:")
+    tags_found = search_by_tag(inputed)
+    print(tags_found)
+
+
+def search_categories():
+    inputed = input("Digite a categoria:")
+    tags_found = search_by_category(inputed)
+    print(tags_found)
+
+
+def search_top_5_news():
+    print(top_5_news())
+
+
+def search_top_5_categories():
+    print(top_5_categories())
+
+
+def scrape_news():
+    inputed = None
+    while not isinstance(inputed, int):
+        try:
+            inputed = int(input("Digite quantas notícias serão buscadas:"))
+        except ValueError:
+            print("Valor digitado deve ser um número inteiro.")
+
+    news_scrapped = get_tech_news(inputed)
+    print(news_scrapped)
+
+
 # Requisito 12
 def analyzer_menu():
+    options = {
+        "0": scrape_news,
+        "1": search_titles,
+        "2": search_dates,
+        "3": search_tags,
+        "4": search_categories,
+        "5": search_top_5_news,
+        "6": search_top_5_categories,
+        "7": end_menu,
+    }
     show_options()
+    try:
+        action = input()
+        options[action]()
+    except KeyError:
+        print("Opção inválida", file=sys.stderr)
