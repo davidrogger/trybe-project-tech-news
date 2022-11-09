3f1a704d3abbcca4587aef52eea61e1a908788f3
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-09 15:24:58 -0300

    feat: start function menu
    create initial menu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,18 @@
+def show_options():
+    print(
+        "Selecione uma das opções a seguir:"
+        "\n 0 - Popular o banco com notícias;"
+        "\n 1 - Buscar notícias por título;"
+        "\n 2 - Buscar notícias por data;"
+        "\n 3 - Buscar notícias por tag;"
+        "\n 4 - Buscar notícias por categoria;"
+        "\n 5 - Listar top 5 notícias;"
+        "\n 6 - Listar top 5 categorias;"
+        "\n 7 - Sair.",
+        end="",
+    )
+
+
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    show_options()
