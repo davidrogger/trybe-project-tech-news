228c38c0cda0f34d61df8530c315469d14b95358
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-09 11:57:19 -0300

    feat: stop 5 news
    to filter the 5 most commented news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,28 @@
+from tech_news.database import get_collection
+from tech_news.analyzer.search_engine import standardized
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    first_criterion = "comments_count"
+    second_criterion = "title"
+    descending_order = -1
+    ascending_order = 1
+
+    db = get_collection()
+
+    news_found = (
+        db.find()
+        .sort(
+            [
+                (first_criterion, descending_order),
+                (second_criterion, ascending_order),
+            ]
+        )
+        .limit(5)
+    )
+
+    return standardized(news_found)
 
 
 # Requisito 11
