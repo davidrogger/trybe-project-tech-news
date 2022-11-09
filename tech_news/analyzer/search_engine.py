defe79ca7c7325396037e5062ae4ddf5935434db
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-09 08:41:29 -0300

    feat: search title
    responsable to bring from the data base the title looked with the url

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    formated_title = title.lower()
+    search_query = {"title": {"$regex": formated_title}}
+
+    titles_found = [
+        tuple([result["title"], result["url"]])
+        for result in search_news(search_query)
+    ]
+
+    return titles_found
 
 
 # Requisito 7
