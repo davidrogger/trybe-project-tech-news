c318d510c1b6c9de19fcc7c5e324f58f4d6a4899
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-09 09:36:46 -0300

    feat: search by date
    responsable to bring from the database the title with the url found by date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,9 @@
 from tech_news.database import search_news
+from datetime import datetime
+
+
+def standardized(query_result):
+    return [tuple([result["title"], result["url"]]) for result in query_result]
 
 
 # Requisito 6
@@ -6,17 +11,26 @@ def search_by_title(title):
     formated_title = title.lower()
     search_query = {"title": {"$regex": formated_title}}
 
-    titles_found = [
-        tuple([result["title"], result["url"]])
-        for result in search_news(search_query)
-    ]
+    titles_found = search_news(search_query)
 
-    return titles_found
+    return standardized(titles_found)
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        valid_format = "%Y-%m-%d"
+        datetime.strptime(date, valid_format)
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        year, month, day = date.split("-")
+        db_date_format = f"{day}/{month}/{year}"
+
+        search_query = {"timestamp": db_date_format}
+        dates_found = search_news(search_query)
+
+        return standardized(dates_found)
 
 
 # Requisito 8
